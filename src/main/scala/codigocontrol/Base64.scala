50e04b0f0e9576ae42f8368849e825f2d2ab3f94
Cherry2 <Cherry2>
2016-07-28 01:42:56 -0400

    begin base64

---
+++ src/main/scala/codigocontrol/Base64.scala
@@ -0,0 +1,28 @@
+package codigocontrol
+
+
+class Base64 {
+
+  def obtenerBase64(value: Int): String = {
+    var varValue = value
+    val diccionario: Array[String] = Array("0", "1", "2", "3", "4", "5", "6", "7", "8",
+      "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
+      "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
+      "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i",
+      "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+      "v", "w", "x", "y", "z", "+", "/")
+
+    var cociente: Int = 1
+    var resto: Int = 0
+    var palabra: String = ""
+    while (cociente > 0) {
+      {
+        cociente = varValue / 64
+        resto = varValue % 64
+        palabra = diccionario(resto) + palabra
+        varValue = cociente
+      }
+    }
+    return palabra
+  }
+}
---
+++ src/test/scala/TestVerhoeff.scala
@@ -0,0 +1,35 @@
+import collection.mutable.Stack
+import org.scalatest._
+import codigocontrol.Verhoeff
+
+class TestVerhoeff extends FlatSpec {
+
+  "1. DigitoVerificador = ObtenerVerhoeff(12083)" should "Resultado: DigitoVerificador = 7" in {
+    val verhoeff = (new Verhoeff).obtenerVerhoeff("12083")
+    val stack = new Stack[Int]
+    stack.push(verhoeff)
+    assert(stack.pop() === 7)
+  }
+
+  "2. DigitoVerificador = ObtenerVerhoeff(0)" should "Resultado: DigitoVerificador = 1" in {
+    val verhoeff = (new Verhoeff).obtenerVerhoeff("0")
+    val stack = new Stack[Int]
+    stack.push(verhoeff)
+    assert(stack.pop() === 1)
+  }
+
+  "3. DigitoVerificador = ObtenerVerhoeff(1810)" should "Resultado: DigitoVerificador = 8" in {
+    val verhoeff = (new Verhoeff).obtenerVerhoeff("1810")
+    val stack = new Stack[Int]
+    stack.push(verhoeff)
+    assert(stack.pop() === 8)
+  }
+
+  "4. DigitoVerificador = ObtenerVerhoeff(04)" should "Resultado: DigitoVerificador = 7" in {
+    val verhoeff = (new Verhoeff).obtenerVerhoeff("04")
+    val stack = new Stack[Int]
+    stack.push(verhoeff)
+    assert(stack.pop() === 7)
+  }
+
+}
