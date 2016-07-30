44d470beb3080693541ac94338c6f628b8ae45d2
Cherry2 <Cherry2>
2016-07-30 11:34:42 -0400

    Refactor codigo control

--- src/main/scala/codigocontrol/CodigoControl.scala
+++ src/main/scala/codigocontrol/CodigoControl.scala
@@ -1,24 +1,37 @@
 package codigocontrol
 
-
-/**
-  * V 7
-  */
 class CodigoControl(var numeroAutorizacion: String, var numeroFactura: String, var nitCi: String,
                     var fechaTransacion: String, var montoTransaccion: String, var llaveDosificacion:String) {
 
   def generar(): String = {
     montoTransaccion = round(montoTransaccion)
 
-    println("PASO 1 ----------------------------------")
-    println("Nro de autorizacion " + numeroAutorizacion)
-    println("Nro Factura " + numeroFactura)
-    println("NIT " + nitCi)
-    println("Fecha de transaccion " + fechaTransacion)
-    println("Monto de transaccion " + montoTransaccion)
-    println("Llave de dosificacion " + llaveDosificacion)
+    // Paso 1
+    val sumatoria5Verhoeff = tratamientoVerhoeff()
 
-      // Paso 1 - obtener y concatenar consecutivamente 2 digitos verhoeff
+    // Paso 2
+    val _5DigitosVerhoeff = sumatoria5Verhoeff.substring(sumatoria5Verhoeff.size-5)
+    val suma1Verhoeff = _5DigitosVerhoeff.split("").map(_.toInt+1)
+
+    // Paso 3
+    val cadenaConcatenada = tratamientoLlaveDosificacion(suma1Verhoeff)
+    val llaveCifrado = llaveDosificacion + _5DigitosVerhoeff
+
+    val claveAllegedRC4: String = (new AllegedRC4).CifrarMensajeRC4(cadenaConcatenada,llaveCifrado).filter(c => c != '-')
+
+    // Paso 4 / 5
+    val claveAllegerRC4Array: Array[Char] = claveAllegedRC4.split("").toArray.map(_.toCharArray).flatten
+
+    val sumaProducto: Long = sumaProductoAscii(claveAllegerRC4Array, suma1Verhoeff)
+    val base64 = (new Base64).obtenerBase64(sumaProducto)
+
+    // Paso 6
+    val allegedRC4 = (new AllegedRC4).CifrarMensajeRC4(base64,llaveCifrado)
+    allegedRC4
+  }
+
+  private def tratamientoVerhoeff(): String = {
+    // Paso 1 - obtener y concatenar consecutivamente 2 digitos verhoeff
     numeroFactura = agregarNVerhoeff(numeroFactura,2)
     nitCi = agregarNVerhoeff(nitCi,2)
     fechaTransacion = agregarNVerhoeff(fechaTransacion,2)
@@ -26,70 +39,21 @@ class CodigoControl(var numeroAutorizacion: String, var numeroFactura: String, v
 
     // sumar
     val sumatoria = numeroFactura.toLong +
-                    nitCi.toLong +
-                    fechaTransacion.toLong +
-                    montoTransaccion.toLong
+      nitCi.toLong +
+      fechaTransacion.toLong +
+      montoTransaccion.toLong
 
-    // agregar 5 digitos Verhoeff a la sumatoria
-    val sumatoria5Verhoeff = agregarNVerhoeff(sumatoria.toString,5)
+    // agregar 5 digitos Verhoeff y retornar
+    agregarNVerhoeff(sumatoria.toString,5)
+  }
 
-    println(" - Vernoheff -")
-    println("Nro de autorizacion " + numeroAutorizacion)
-    println("Nro Factura " + numeroFactura)
-    println("NIT " + nitCi)
-    println("Fecha de transaccion " + fechaTransacion)
-    println("Monto de transaccion " + montoTransaccion)
-    println("Suma  " + sumatoria5Verhoeff)
-
-    // Paso 2
-    println("PASO 2 ----------------------------------")
-    val _5DigitosVerhoeff = sumatoria5Verhoeff.substring(sumatoria5Verhoeff.size-5)
-    println(" _5DigitosVerhoeff " + _5DigitosVerhoeff)
-    // largo de las cadenas, suma 1 a cada digito verhoeff
-    val suma1Verhoeff = _5DigitosVerhoeff.split("").map(_.toInt+1)
-    println("suma1Verhoeff " + suma1Verhoeff.foldLeft("")((a,b) => {a + "" + b }))
-
-    val substDosificacion1 = llaveDosificacion.substring(0, suma1Verhoeff(0))
-    val substDosificacion2 = llaveDosificacion.substring(suma1Verhoeff(0), suma1Verhoeff(0) + suma1Verhoeff(1))
-    val substDosificacion3 = llaveDosificacion.substring(suma1Verhoeff(0) + suma1Verhoeff(1),
-      suma1Verhoeff(0)+suma1Verhoeff(1) + suma1Verhoeff(2))
-    val substDosificacion4 = llaveDosificacion.substring(suma1Verhoeff(0)+suma1Verhoeff(1) + suma1Verhoeff(2),
-      suma1Verhoeff(0)+suma1Verhoeff(1) + suma1Verhoeff(2) + suma1Verhoeff(3))
-    val substDosificacion5 = llaveDosificacion.substring(suma1Verhoeff(0)+suma1Verhoeff(1) + suma1Verhoeff(2) +
-      suma1Verhoeff(3), suma1Verhoeff(0)+suma1Verhoeff(1) + suma1Verhoeff(2) + suma1Verhoeff(3) + suma1Verhoeff(4))
-
-    println("substDosificacion 1 " + substDosificacion1)
-    println("substDosificacion 2 " + substDosificacion2)
-    println("substDosificacion 3 " + substDosificacion3)
-    println("substDosificacion 4 " + substDosificacion4)
-    println("substDosificacion 5 " + substDosificacion5)
-
-    numeroAutorizacion = numeroAutorizacion + substDosificacion1
-    numeroFactura = numeroFactura + substDosificacion2
-    nitCi = nitCi + substDosificacion3
-    fechaTransacion = fechaTransacion + substDosificacion4
-    montoTransaccion = montoTransaccion + substDosificacion5
-
-    println("numeroAutorizacion  " + numeroAutorizacion)
-    println("numeroFactura " + numeroFactura)
-    println("nitCi  " + nitCi)
-    println("fechaTransacion " + fechaTransacion)
-    println("montoTransaccion " + montoTransaccion)
-
-    // Paso 3
-    println("PASO 3 ----------------------------------")
-    val cadenaConcatenada = numeroAutorizacion + numeroFactura + nitCi + fechaTransacion + montoTransaccion
-    val llaveCifrado = llaveDosificacion + _5DigitosVerhoeff
-
-    println(" cadena concatenada " + cadenaConcatenada)
-    println(" llave de cifrado " +  llaveCifrado)
-
-    val claveAllegedRC4: String = (new AllegedRC4).CifrarMensajeRC4(cadenaConcatenada,llaveCifrado).filter(c => c != '-')
-    println(" claveAllegedRC4 " + claveAllegedRC4)
-
-    // Paso 4
-    val claveAllegerRC4Array: Array[Char] = claveAllegedRC4.split("").toArray.map(_.toCharArray).flatten
-
+  /**
+    * Paso 4 y 5
+    * @param claveAllegerRC4Array
+    * @param suma1Verhoeff
+    * @return
+    */
+  private def sumaProductoAscii(claveAllegerRC4Array: Array[Char], suma1Verhoeff: Array[Int]): Long = {
     var sumatoriaTotal = 0
     var sumatoriaParcial1 = 0
     var sumatoriaParcial2 = 0
@@ -117,41 +81,43 @@ class CodigoControl(var numeroAutorizacion: String, var numeroFactura: String, v
       cont = if(cont<5) cont+1 else 1
     })
 
-    println(" sumatoria total " + sumatoriaTotal)
-    println(" sumatoria parcial 1 " + sumatoriaParcial1)
-    println(" sumatoria parcial 2 " + sumatoriaParcial2)
-    println(" sumatoria parcial 3 " + sumatoriaParcial3)
-    println(" sumatoria parcial 4 " + sumatoriaParcial4)
-    println(" sumatoria parcial 5 " + sumatoriaParcial5)
 
     // Paso 5
-    println(" paso 5 --------------------- ")
     val stParcial1: Long = sumatoriaTotal * sumatoriaParcial1 / suma1Verhoeff(0)
     val stParcial2: Long = sumatoriaTotal * sumatoriaParcial2 / suma1Verhoeff(1)
     val stParcial3: Long = sumatoriaTotal * sumatoriaParcial3 / suma1Verhoeff(2)
     val stParcial4: Long = sumatoriaTotal * sumatoriaParcial4 / suma1Verhoeff(3)
     val stParcial5: Long = sumatoriaTotal * sumatoriaParcial5 / suma1Verhoeff(4)
 
-    println(" stParcial1 " + stParcial1)
-    println(" stParcial2 " + stParcial2)
-    println(" stParcial3 " + stParcial3)
-    println(" stParcial4 " + stParcial4)
-    println(" stParcial5 " + stParcial5)
 
     val sumaProducto: Long = (stParcial1 + stParcial2 + stParcial3
       + stParcial4 + stParcial5)
 
-    println(" sumaProducto  " + sumaProducto)
+    sumaProducto
+  }
 
-    val base64 = (new Base64).obtenerBase64(sumaProducto)
+  /**
+    * @param suma1Verhoeff
+    * realiza el tratamiento de llave de dosificacion correspondiente al paso 2
+    */
+  private def tratamientoLlaveDosificacion(suma1Verhoeff: Array[Int]): String = {
 
-    println(" base 64 " + base64)
+    val substDosificacion1 = llaveDosificacion.substring(0, suma1Verhoeff(0))
+    val substDosificacion2 = llaveDosificacion.substring(suma1Verhoeff(0), suma1Verhoeff(0) + suma1Verhoeff(1))
+    val substDosificacion3 = llaveDosificacion.substring(suma1Verhoeff(0) + suma1Verhoeff(1),
+      suma1Verhoeff(0)+suma1Verhoeff(1) + suma1Verhoeff(2))
+    val substDosificacion4 = llaveDosificacion.substring(suma1Verhoeff(0)+suma1Verhoeff(1) + suma1Verhoeff(2),
+      suma1Verhoeff(0)+suma1Verhoeff(1) + suma1Verhoeff(2) + suma1Verhoeff(3))
+    val substDosificacion5 = llaveDosificacion.substring(suma1Verhoeff(0)+suma1Verhoeff(1) + suma1Verhoeff(2) +
+      suma1Verhoeff(3), suma1Verhoeff(0)+suma1Verhoeff(1) + suma1Verhoeff(2) + suma1Verhoeff(3) + suma1Verhoeff(4))
 
-    // paso 6
-    println(" paso 6 --------------------- ")
-    val allegedRC4 = (new AllegedRC4).CifrarMensajeRC4(base64,llaveCifrado)
-    println(" alleged " + allegedRC4)
-    allegedRC4
+    numeroAutorizacion = numeroAutorizacion + substDosificacion1
+    numeroFactura = numeroFactura + substDosificacion2
+    nitCi = nitCi + substDosificacion3
+    fechaTransacion = fechaTransacion + substDosificacion4
+    montoTransaccion = montoTransaccion + substDosificacion5
+
+    numeroAutorizacion + numeroFactura + nitCi + fechaTransacion + montoTransaccion
   }
 
   private def agregarNVerhoeff(value: String, n: Int): String = {
--- src/main/scala/sin/Sin.scala
+++ src/main/scala/sin/Sin.scala
@@ -4,6 +4,7 @@ import java.io.{BufferedReader, FileReader}
 import codigocontrol.CodigoControl
 
 object FacturacionSIN extends App {
+
   val codigoControl: CodigoControl = null
   val file: String = "C:\\IdeaProjects\\FacturacionSIN\\doc\\5000CasosPruebaCCVer7.txt"
   var cont =0
@@ -19,11 +20,13 @@ object FacturacionSIN extends App {
       //reemplaza "|" por "/-/" por no ser compatible con el metodo split
       val array = linea.replace("|", "/-/").split("/-/").filter(v => v != "/-/")
 
-      println(" linea" + linea)
-      println(" array" + array.foldLeft("")((a,b) => {a + "   " + b }))
-      println("size" + array.size)
-      val codigoControl = new CodigoControl(array(0), array(1), array(2), array(3).replace("/", ""), array(4), array(5)).generar()
-      println(" nro:" + cont + " CODIGO CONTROL: " + codigoControl)
+      val codigoControlGenerado = new CodigoControl(array(0), array(1), array(2), array(3).replace("/", ""),
+        array(4), array(5)).generar()
+      val codigoControlLeido = array(10)
+
+      println(cont + " Codigo de control leido: " + codigoControlLeido +
+        " Codigo de control generado " + codigoControlGenerado + " iguales -> " +
+        (codigoControlGenerado == codigoControlLeido) )
 
     }
   } catch {
